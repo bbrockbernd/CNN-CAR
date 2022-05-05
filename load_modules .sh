c4db99678ddab94937af548143c092f3ed1c1f98
Bob Brockbernd <bobbrockbernd@gmail.com>
2022-05-05 10:30:19 +0200

    added module loader

---
+++ load_modules .sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+module load gpu 2022r1
+module load gpu
+module load git/2.31.1-xempvss
+module load python/3.8.12-bohr45d
+module load openssh/8.7p1-ftn4ah4
+module load miniconda3/4.10.3-buki4rd
