37e12390a549a7ccbcc10579015f777b4c61630a
Bob Brockbernd <bobbrockbernd@gmail.com>
2022-05-09 14:17:23 +0200

    tf module trough slurm modules

--- load_modules.sh
+++ load_modules.sh
@@ -9,6 +9,7 @@ module load openssh/8.7p1-ftn4ah4
 module load py-pip/21.1.2-zxgv7pz
 module load cudnn/8.0.5.39-11.1-oxf3pke
 module load cuda11.2/toolkit/11.2.2
-
+module load py-tensorflow/2.4.1-b6kuk57
+module load openmpi/4.1.1-i2mystw
 
 #module load miniconda3/4.10.3-buki4rd
