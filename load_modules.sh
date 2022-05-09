b60ad273af1009adc15115c185a8a3d5953f36c1
Bob Brockbernd <bobbrockbernd@gmail.com>
2022-05-09 13:09:32 +0200

    added modules

--- load_modules.sh
+++ load_modules.sh
@@ -6,3 +6,6 @@ module load git/2.31.1-xempvss
 module load python/3.8.12-bohr45d
 module load openssh/8.7p1-ftn4ah4
 module load miniconda3/4.10.3-buki4rd
+module load py-pip/21.1.2-zxgv7pz
+module load cudnn/8.0.5.39-11.1-oxf3pke
+module load cuda11.2/toolkit/11.2.2
