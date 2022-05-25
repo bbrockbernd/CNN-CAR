fa0f46153dc081fa3882837a0cbec5f3aaaf210e
Bob Brockbernd <bobbrockbernd@gmail.com>
2022-05-25 15:11:57 +0200

    adding conditional layers to 1d

--- Model1D.py
+++ Model1D.py
@@ -10,23 +10,47 @@ import keras_tuner as kt
 class HyperModel1D(kt.HyperModel):
 
     def build(self, hp):
+        frame_length = hp.Int('frame length', 32, 512, 32)
+
+        n_filters_1 = hp.Int('filter count 1', 8, 128, 8)
+        kernel_size_1 = hp.Int('kernel size 1', 3, 17, 2)
+        drop_1 = hp.Float('drop out 1', 0.6, 1.0, 0.2)
+        pool_1 = hp.Choice('max pool 1', [2, 4, 8, 16])
+
+        second_convolution = hp.Boolean('Enable 2nd Conv')
+        n_filters_2 = hp.Int('filter count 2', 8, 128, 8, parent_name='Enable 2nd Conv', parent_values=[True])
+        kernel_size_2 = hp.Int('kernel size 2', 3, 17, 2, parent_name='Enable 2nd Conv', parent_values=[True])
+        drop_2 = hp.Float('drop out 2', 0.6, 1.0, 0.2, parent_name='Enable 2nd Conv', parent_values=[True])
+        pool_2 = hp.Choice('max pool 2', [2, 4, 8, 16], parent_name='Enable 2nd Conv', parent_values=[True])
+
+
+        dense_size_1 = hp.Int('Dense size 1', 60, 200, 20)
+        drop_3 = hp.Float('drop out 3', 0.6, 1.0, 0.2)
+
+        second_dense = hp.Boolean('Enable 2nd Dense')
+        dense_size_2 = hp.Int('Dense size 2', 60, 200, 20, parent_name='Enable 2nd Dense', parent_values=[True])
+        drop_4 = hp.Float('drop out 4', 0.6, 1.0, 0.2, parent_name='Enable 2nd Dense', parent_values=[True])
+
+
         model = keras.Sequential()
 
-        n_filters = hp.Int('filter count', 8, 128, 8)
-        kernel_size = hp.Int('kernel size', 3, 17, 2)
-        frame_length = hp.Int('frame length', 32, 512, 32)
-        pool_1 = hp.Choice('max pool 1', [2, 4, 8, 16])
-        dense_size = hp.Int('Dense size', 60, 200, 20)
-
-        drop_1 = hp.Float('drop out 1', 0.6, 1.0, 0.2)
-        drop_2 = hp.Float('drop out 2', 0.6, 1.0, 0.2)
-
-        model.add(layers.Conv1D(filters=n_filters, kernel_size=kernel_size, activation='relu', input_shape=(frame_length, 2), padding='same'))
+        model.add(layers.Conv1D(filters=n_filters_1, kernel_size=kernel_size_1, activation='relu', input_shape=(frame_length, 2), padding='same'))
         model.add(layers.Dropout(drop_1))
         model.add(layers.MaxPooling1D(pool_size=pool_1))
+
+        if second_convolution:
+            model.add(layers.Conv1D(filters=n_filters_2, kernel_size=kernel_size_2, activation='relu', padding='same'))
+            model.add(layers.Dropout(drop_2))
+            model.add(layers.MaxPooling1D(pool_size=pool_2))
+
         model.add(layers.Flatten())
-        model.add(layers.Dense(dense_size, activation='relu'))
-        model.add(layers.Dropout(drop_2))
+        model.add(layers.Dense(dense_size_1, activation='relu'))
+        model.add(layers.Dropout(drop_3))
+
+        if second_dense:
+            model.add(layers.Dense(dense_size_2, activation='relu'))
+            model.add(layers.Dropout(drop_4))
+
         model.add(layers.Dense(8, activation='softmax'))
 
         model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
@@ -83,11 +107,17 @@ def tune_params():
                             overwrite=True,
                             directory="tuning_tester",
                             project_name="tune_hypermodel")
-    tuner.search(epochs=1)
+
+    callback = keras.callbacks.EarlyStopping(monitor='val_loss',
+                                  min_delta=1e-4,
+                                  patience=2,
+                                  verbose=0, mode='auto')
+
+    tuner.search(epochs=30, callbacks=[callback])
 
 
-loader = DataLoader(DataSet.SEDENTARY)
-trainX, trainy, testX, testy = loader.load_1D()
-evaluate_model(trainX, trainy, testX, testy)
+# loader = DataLoader(DataSet.SEDENTARY)
+# trainX, trainy, testX, testy = loader.load_1D()
+# evaluate_model(trainX, trainy, testX, testy)
 
-# tune_params()
+tune_params()
